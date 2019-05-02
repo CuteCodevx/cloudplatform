4aeda94a82a939620f8bf911b33e88816493a926
ivy <609220610@qq.com>
2019-05-02 11:24:38 +0100

    fixed ajax success function about login and register second time

--- src/main/java/com/sheffield/login/controller/AppUploadController.java
+++ src/main/java/com/sheffield/login/controller/AppUploadController.java
@@ -8,6 +8,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.sheffield.login.service.ApplicationService;
 
-
+@Controller
 public class AppUploadController {
 
     @Resource
